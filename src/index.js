f4659f76e486f6d3d2bdb8523b679db8e4c703c2
subrunner <susanne.brunner@edcom.de>
2023-08-28 15:17:32 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyAl7ZbDVhXFAyZpEzQ6HdaQzyzaWIYXOzQ",
+
+  authDomain: "timemanager-945bf.firebaseapp.com",
+
+  databaseURL: "https://timemanager-945bf-default-rtdb.europe-west1.firebasedatabase.app",
+
+  projectId: "timemanager-945bf",
+
+  storageBucket: "timemanager-945bf.appspot.com",
+
+  messagingSenderId: "737000542196",
+
+  appId: "1:737000542196:web:7ad39b9c0f34cf002e73a1"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
